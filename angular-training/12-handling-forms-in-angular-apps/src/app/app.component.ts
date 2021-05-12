8483a2f7b7cddf318669bfc89601a30f0be1eba8
shashank.raj@novopay.in <52448329+shashank1712@users.noreply.github.com>
2021-05-12 11:28:59 +0530

    Day 12: Handling Forms in Angular Apps

---
+++ angular-training/12-handling-forms-in-angular-apps/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm;
+
+  genders = ['male', 'female'];
+  defaultQuestion = "pet";
+  answer = '';
+  submitted = false;
+
+  user = {
+    username: '',
+    mail: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm): void {
+  //   console.log(form);
+  // }
+
+  onSubmit(): void {
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.mail = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+}
